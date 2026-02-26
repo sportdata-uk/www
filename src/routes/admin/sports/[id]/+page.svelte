e590b488354a216b41d519534f24a905f6b1e04a
Richard Herbert <richard@infoweb.co.uk>
2026-02-26 16:39:43 +0000

    Updated form

--- src/routes/admin/sports/[id]/+page.svelte
+++ src/routes/admin/sports/[id]/+page.svelte
@@ -14,11 +14,10 @@
 <div class="row">
 	<div class="col-8 offset-2">
 		<form action="?/update" method="POST">
-			<label for="name">Name *
+			<label for="name" data-field>Name *
 				<input id="name" name="name" type="text" value={ data.sports?.name || '' } required/>
 			</label>
 
-			<br>
 			<button>{ data.sports.id ? 'Update' : 'Add' } sport</button>
 			<a href="/admin/sports/" class="button outline">cancel</a>
 		</form>
